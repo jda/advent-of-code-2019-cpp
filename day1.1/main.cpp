e8bcfaa1b4c12326b1eaa69c286d4f9f4b7b494e
Jade Auer <jade@trashwitch.dev>
2019-12-01 16:35:48 -0500

    day1 part 1 complete

---
+++ day1.1/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <math.h>
+
+int findfuel(int mass) {
+    std::cout << "Mass: " << mass << std::endl;
+    float stage1 = mass / (1.0 * 3);
+    std::cout << "stage1: " << stage1 << std::endl;
+    int stage2 = floor(stage1);
+    std::cout << "stage2: " << stage2 << std::endl;
+    int stage3 = stage2 - 2;
+    std::cout << "stage3: " << stage3 << std::endl;
+    return stage3;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <FILE>" << std::endl;
+        return 1;
+    }
+
+    std::string infileName = argv[1];
+
+    std::ifstream infile;
+    infile.open(infileName);
+    if (!infile.is_open()) {
+        std::cerr << "Could not open file " << infileName << std::endl;
+        return 1;
+    }
+
+    int fuelreq = 0;
+    std::string line;
+    
+    while (std::getline(infile, line)) {
+        // skip blank
+        if (line.empty()) {
+            continue;
+        }
+
+        // skip commented
+        if (line.rfind("#") == 0) {
+            continue;
+        }
+
+        std::string rawPartMass (line.substr(0));
+        int partMass = std::stoll(rawPartMass);
+
+        fuelreq += findfuel(partMass);
+
+    }
+    infile.close();
+
+    std::cout << "Fuel required: " << fuelreq << std::endl;
+}
